9e319afe44233115a3033fd1353e3d4195a15179
Szajszka <arkadiusz.pradzinski@gmail.com>
2022-01-20 18:54:39 +0100

    Added a try/except clause to handle the case of no availability to put a price of 0 into the table instead of stopping the entire script

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from sqlSocket import (getTable, sqlInput)
 
 url_list = list(fileReader("urls.txt."))
 for i in range(0, len(url_list), 4):
-    sqlInput(url_list[i+3], url_list[i+2], scrapper(url_list[i+1]))
+    try:
+        sqlInput(url_list[i+3], url_list[i+2], scrapper(url_list[i+1]))
+    except:
+        sqlInput(url_list[i+3], url_list[i+2], 0)
 
 # debug, delete in final build
 getTable()
